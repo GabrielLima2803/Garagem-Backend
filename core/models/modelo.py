f2fcaac7793f2c0a73183ce4336b1bbb69eccfbb
GabrielLima2803 <gabriellima2803@gmail.com>
2024-07-09 20:46:18 -0300

    fix: related_name corrigido e tabela criada

--- core/models/modelo.py
+++ core/models/modelo.py
@@ -5,8 +5,8 @@ from .categoria import Categoria
 
 class Modelo(models.Model):
     nome = models.CharField(max_length=100)
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="marcas")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="categorias")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="modelos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="modelos")
 
     def __str__(self):
         return f"{self.nome} - {self.marca}"

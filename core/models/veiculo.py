48f06ca6aaf064b03449c70ad8a4f091377ac071
GabrielLima2803 <gabriellima2803@gmail.com>
2024-07-09 20:52:49 -0300

    Fix: Alterando de on_delete Protect para Cascade

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -5,8 +5,8 @@ from .cor import Cor
 from .acessorio import Acessorio
 
 class Veiculo(models.Model):
-    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE, related_name="veiculos")
     ano = models.IntegerField(default=0, null=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
     acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")

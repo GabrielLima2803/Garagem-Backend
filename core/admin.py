93795efca77b88a8291d0830455bce360571339c
GabrielLima2803 <gabriellima2803@gmail.com>
2024-07-16 15:17:26 -0300

    feat: Aula sobre o admin e o passage id

--- core/admin.py
+++ core/admin.py
@@ -29,6 +29,7 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login",)}),
         (_("Groups"), {"fields": ("groups",)}),
+        (_("User Permission"), {"fields": ("user_permissions",)})
     )
     readonly_fields = ["last_login"]
     add_fieldsets = (
